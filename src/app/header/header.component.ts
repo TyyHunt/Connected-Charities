da3b52604b7f2c48bad19bc28fd39bf3afcc04af
Tyler Hunt <47220891+TyyHunt@users.noreply.github.com>
2020-09-22 23:18:08 -0600

    navbar and header working as intended

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  public logoImgPath = "/assets/imgs/logo2.png";
 
   constructor() { }
 
