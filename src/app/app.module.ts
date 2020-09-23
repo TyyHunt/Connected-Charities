a96c1662a1a9e19197e0ee67acce7a34a75de664
Tyler Hunt <47220891+TyyHunt@users.noreply.github.com>
2020-09-22 21:51:55 -0600

    added bootstrap to project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {MatSidenavModule} from '@angular/material/sidenav';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
@@ -15,7 +14,7 @@ import { MenuListComponent } from './menu-list/menu-list.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatSidenavModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
